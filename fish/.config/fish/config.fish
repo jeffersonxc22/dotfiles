82499380ea4256fbae1ae0c8b0fee9b7ee42eff1
Jefferson Xenofonte Cardoso <jeffersonxc.22@gmail.com>
2026-04-11 14:22:16 -0300

    Saphire Cattpuccin

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,11 +1,34 @@
+# Carrega config base do CachyOS
 source /usr/share/cachyos-fish-config/cachyos-config.fish
 
-# overwrite greeting
-# potentially disabling fastfetch
-#function fish_greeting
-#    # smth smth
-#end
-
-#Starshi Initiation
+# Starship
 starship init fish | source
+
+# Zoxide
+zoxide init fish | source
+
+# FZF — key bindings e fuzzy completion
+fzf --fish | source
+
+# mise
+mise activate fish | source
+
+# Variáveis de ambiente
+set -gx EDITOR nvim
+set -gx SUDO_EDITOR nvim
+set -gx PGHOST /var/run/postgresql
+
+# PATH
+fish_add_path /usr/local/go/bin
+
+
+#Init TMUX
+if not set -q TMUX
+    if tmux has-session 2>/dev/null
+        exec tmux attach
+    else
+        exec tmux
+    end
+end
+
 
--- nvim/.config/nvim/lua/plugins/catppuccin.lua
+++ nvim/.config/nvim/lua/plugins/catppuccin.lua
@@ -4,10 +4,14 @@ return {
     lazy = false,
     name = "catppuccin",
     priority = 1000,
-
     config = function()
       require("catppuccin").setup({
         transparent_background = true,
+        color_overrides = {
+          mocha = {
+            mauve = "#74c7ec", -- redireciona mauve → sapphire
+          },
+        },
       })
       vim.cmd.colorscheme "catppuccin-mocha"
     end
