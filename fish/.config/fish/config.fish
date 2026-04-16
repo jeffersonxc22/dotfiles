a3cbc181ab6ec40c0c06732ac492eca9436983a5
Jefferson Xenofonte <jeffersonxc.22@gmail.com>
2026-04-16 16:38:43 -0300

    Customizer Catppuccion in enverionment Hyprland

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -18,6 +18,13 @@ set -gx EDITOR nvim
 set -gx SUDO_EDITOR nvim
 set -gx PGHOST /var/run/postgresql
 
+# Garante que o ssh-agent está rodando e com a chave carregada
+if not set -q SSH_AUTH_SOCK
+    eval (ssh-agent -c) > /dev/null
+    ssh-add ~/.ssh/id_ed25519 2> /dev/null
+end
+
+
 # PATH
 fish_add_path /usr/local/go/bin
 fish_add_path ~/.local/bin
